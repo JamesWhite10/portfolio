750e589f74a97e9904891aa083f0cf7c56ea165e
JamesWhite10 <max.migalin10@gmail.com>
2021-07-26 21:42:58 +0300

    Refactoring.

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -6,14 +6,14 @@ export const Footer = () => {
     return (
         <div className={style.footerBlock}>
             <div className={`${styleContainer.container} ${style.footerContainer}`}>
-                <h3 className={style.title}>Maxim Migalin</h3>
+                <h2 className={style.title}>Maxim Migalin</h2>
              <div className={style.socialContainer}>
-                 <span className={style.icon}></span>
-                 <span className={style.icon}></span>
-                 <span className={style.icon}></span>
-                 <span className={style.icon}></span>
+                 <span className={style.icon}><img className={style.image} src={"https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" } alt={"javaScript"}/></span>
+                 <span className={style.icon}><img className={style.image} src={"https://avatars.githubusercontent.com/u/6412038?s=200&v=4"} alt={"react"}/></span>
+                 <span className={style.icon}><img className={style.image} src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgWLBe-VaJIYEbmTPrm1XyFy35MInioRpbpKK48hmPbj-m8TCYEG_GSIUZZIpXNuTqG-U&usqp=CAU"} alt={"redux"}/></span>
+                 <span className={style.icon}><img className={style.image} src={"https://iconape.com/wp-content/png_logo_vector/typescript.png"} alt={"typeScript"}/></span>
              </div>
-                <p>&copy; All rights reserved</p>
+                <p className={style.rights}>&copy; All rights reserved</p>
             </div>
         </div>
     )
