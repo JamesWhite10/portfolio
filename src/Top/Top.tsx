abcc9dac7ba1ce269f0f543a60ec46197d6441c7
JamesWhite10 <max.migalin10@gmail.com>
2021-08-02 15:31:47 +0300

    Refactoring.

---
+++ src/Top/Top.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import style from "./Top.module.scss";
+
+
+export const Top = () => {
+    return (
+        <div className={style.top}>
+            <a href={"#nav"}>Top</a>
+        </div>
+    )
+}
